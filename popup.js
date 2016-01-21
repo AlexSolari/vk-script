abdf263796bb180e60d8c5df007db51c25f45f97
AlexSolari <alexsanify@gmail.com>
2016-01-21 21:24:39 +0200

    Implemented "Find a pic" feature

--- popup.js
+++ popup.js
@@ -2,8 +2,7 @@ function Settings() {
     this.AutohideCommentsSpells = [];
     this.AutohideCommentsWithImages = false;
     this.AutohideCommentsWithAudio = false;
-    this.AutohideCommentsInterval = 0;
-    this.AutohideMessagesInterval = 0;
+    this.Interval = 0;
 }
 
 Settings.prototype.Load = function () {
@@ -12,7 +11,7 @@ Settings.prototype.Load = function () {
     $("input[name='audio-filter']")[0].checked = JSON.parse(window.localStorage["vkscript-comments-audioshide"]);
 }
 
-Settings.prototype.Save = function () {
+Settings.prototype.SaveComments = function () {
     window.localStorage["vkscript-comments-spells"] = $(".comment-autohide")[0].value;
     window.localStorage["vkscript-comments-imageshide"] = $("input[name='image-filter']")[0].checked;
     window.localStorage["vkscript-comments-audioshide"] = $("input[name='audio-filter']")[0].checked;
@@ -27,6 +26,7 @@ Settings.prototype.Save = function () {
 }
 
 var confing;
+var currentImage;
 
 $(document).ready(function () {
     var wrapper = $('<div id="vkscript-wrapper"></div>');
@@ -47,41 +47,46 @@ $(document).ready(function () {
         });
 
         $("#comment-autohide-apply").click(function () {
-            AutohideCommentsApply();
+            confing.SaveComments();
         });
 
+        confing.Interval = setInterval(ProcessScript, 200);
+
         confing.Load();
-        AutohideCommentsApply();
+        confing.SaveComments();
+
     });
 });
 
-
-
-function AutohideCommentsApply() {
+function ProcessScript() {
+    /*Comments*/
     $("*").removeClass("script-hidden");
-    
-    confing.Save();
+    confing.AutohideCommentsSpells.forEach(function (word) {
+        $(".fw_reply, .reply").each(function (index, element) {
+            var $el = $(element).find(".fw_reply_text, .wall_reply_text, .reply_text");
 
-    clearInterval(confing.AutohideCommentsInterval);
-    confing.AutohideCommentsInterval = setInterval(function () {
+            if ($(element).has(".audio").length > 0 && confing.AutohideCommentsWithAudio)
+                $(element).addClass("script-hidden");
 
-        confing.AutohideCommentsSpells.forEach(function (word) {
-            $(".fw_reply, .reply").each(function (index, element) {
-                var $el = $(element).find(".fw_reply_text, .wall_reply_text, .reply_text");
+            if ($(element).has(".page_post_thumb_sized_photo").length > 0 && confing.AutohideCommentsWithImages)
+                $(element).addClass("script-hidden");
 
-                if ($(element).has(".audio").length > 0 && confing.AutohideCommentsWithAudio)
-                    $(element).addClass("script-hidden");
+            if ($el.length > 0 && $el.text().indexOf(word) > -1 && word.length > 0)
+                $(element).addClass("script-hidden");
+        });
 
-                if ($(element).has(".page_post_thumb_sized_photo").length > 0 && confing.AutohideCommentsWithImages)
-                    $(element).addClass("script-hidden");
-
-                if ($el.length > 0 && $el.text().indexOf(word) > -1 && word.length > 0)
-                    $(element).addClass("script-hidden");
-            });
-
-        })
-
+    });
+    /*Images*/
+    if ($("#pv_actions").length > 0)
+    {
+        currentImage = $("#pv_photo img").prop("src");
+        if ($(".vkscript-find").length == 0)
+        {
+            $("#pv_actions").append($('<a class="vkscript-find vkscript-find-yandex" target="_blank" href="http://yandex.ru/images/search?rpt=imageview&img_url=' + currentImage + '"></a>'));
+            $("#pv_actions").append($('<a class="vkscript-find vkscript-find-google" target="_blank" href="http://www.google.com/searchbyimage?image_url=' + currentImage + '&safe=off"></a>'));
+            $("#pv_actions").append($('<a class="vkscript-find vkscript-find-iqdb" target="_blank" href="http://iqdb.org/?url=' + currentImage + '"></a>'));
+        }
+    }
         
-    }, 200);
-}
 
+}
